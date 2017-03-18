a1929da444f7aa0ae58fc8b3394656f3a49ffca1
wextia <valcazar96@gmail.com>
2017-03-19 00:50:02 +0100

    EnemySpawning works

--- Assets/Scripts/Enemy/EnemyLogic.cs
+++ Assets/Scripts/Enemy/EnemyLogic.cs
@@ -7,12 +7,15 @@ using UnityEngine.AI;
 public class EnemyLogic : MonoBehaviour {
 
     [SerializeField]
+    string tagToFollow = "Player";
+
     Transform target;
 
     private NavMeshAgent agent;
 
     void Start()
     {
+        target = GameObject.FindGameObjectWithTag(tagToFollow).transform;
         agent = GetComponent<NavMeshAgent>();
         agent.SetDestination(target.position);
     }

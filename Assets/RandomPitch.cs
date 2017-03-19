47bfa5a4f5871c5b25ece5f09abf6d185b297a4f
wextia <valcazar96@gmail.com>
2017-03-19 04:07:37 +0100

    Added sounds

---
+++ Assets/RandomPitch.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+
+public class RandomPitch : MonoBehaviour {
+    [SerializeField]
+    float minPitch = .1f;
+    [SerializeField]
+    float maxPitch = 1.5f;
+
+    void Awake()
+    {
+        GetComponent<AudioSource>().pitch = Random.Range(minPitch, maxPitch);
+    }
+}

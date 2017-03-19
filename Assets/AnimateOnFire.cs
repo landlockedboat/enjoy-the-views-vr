388414463bd69f9e8d1b9bb7badcee77eda9eef6
wextia <valcazar96@gmail.com>
2017-03-19 05:02:20 +0100

    Pistol animation works

---
+++ Assets/AnimateOnFire.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateOnFire : MonoBehaviour {
+    Animation anim;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animation>();
+        InputHandler.Instance.RegisterOnFireCallback(Animate);
+	}
+
+    void Animate()
+    {
+        if (WeaponHandler.Instance.GetCurrentAmmo() <= 0)
+            return;
+        if(anim.IsPlaying("Shot"))
+            anim.Stop();
+        anim.Play();
+    }
+	
+}
